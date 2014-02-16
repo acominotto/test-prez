a666b0b40a829c01d011019784bbd03467223c95
achetwooh <adrien.cominotto@gmail.com>
2014-02-16 20:13:59 +0100

    first commit

---
+++ test/spec/javascript.js
@@ -0,0 +1,11 @@
+'use strict';
+
+describe('Javascript', function() {
+  it('should add two numbers', function() {
+    expect(1 + 1).toBe(2);
+  });
+  it('should work well with booleans', function() {
+    expect(true).toBeTruthy();
+    expect(true).not.toBeFalsy();
+  });
+});
