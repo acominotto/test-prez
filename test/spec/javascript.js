f6a7668a511e5c83374a809037551535acea30fa
achetwooh <adrien.cominotto@gmail.com>
2014-02-26 16:54:54 +0100

    fixes + tests + qrcode

--- test/spec/javascript.js
+++ test/spec/javascript.js
@@ -3,13 +3,11 @@
 describe('Javascript', function() {
   it('should add two numbers', function() {
     expect(1 + 1).toBe(2);
+    expect(1.1 + 1.9).toBe(3)
   });
   it('should work well with booleans', function() {
     expect(true).toBeTruthy();
     expect(true).not.toBeFalsy();
   });
-  it('should add two floating points', function(){
-    expect(1.1 + 1.9).toBe(3)
-  })
 });
 
